4e4713c7273ba54690f99701b97716b78ca7b6c3
Logan Katz <loganzk@gmail.com>
2016-08-02 19:50:09 -0700

    Working predictor, dict is messed up

--- src/predictor.py
+++ src/predictor.py
@@ -3,16 +3,31 @@ from keras.models import load_model
 import sys
 from scipy.ndimage import imread
 from scipy.misc import imresize
+import json
 
 def predict(img):
+    with open('/Users/lzkatz/Desktop/Galvanize/Capstone/Identifido/aux_files/dog_dict.json') as bd:
+        classes = json.load(bd)
+    print classes
     model_dir = '/Users/lzkatz/Desktop/Galvanize/Capstone/Identifido/model/basic/final_model.hdf5'
     model = load_model(model_dir)
-    img = img.resize
+    img_dim = model.input_shape[2]
+    img_res = imresize(img, (img_dim, img_dim, 3))
+    img_prep = np.array([img_res.T])
+    
+    class_pred = model.predict(img_prep)
+    print class_pred
+    sort_pred = np.argsort(class_pred.flatten())[:-6:-1]
 
+    breed_pred = []
+    for i in sort_pred:
+        breed_pred.append((classes[i], class_pred[i]))
+    
+    return breed_pred
 
 if __name__ == '__main__':
     pic_dir = '/Users/lzkatz/Desktop/Galvanize/Capstone/Identifido/data/test/'
-    model 
     pic = sys.argv[1]
     img = imread(pic_dir + pic)
     results = predict(img)
+    print results
