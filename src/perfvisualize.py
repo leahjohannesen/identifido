2a73f992f637f72fddaadfd2d82f1c32feff6c22
Logan Katz <loganzk@gmail.com>
2016-08-04 11:45:19 -0700

    Working visualization

--- src/perfvisualize.py
+++ src/perfvisualize.py
@@ -15,15 +15,21 @@ def get_breed_labels(arr, bd):
 def cat_acc(path):
     with open('./aux_files/breed_dict.json') as bd:
         breed_dict = json.load(bd)
+    
+    #bunch of figures
+    fig1, ax1 = plt.subplots(1,1)
+    fig2, ax2 = plt.subplots(1,1)
+    fig3, ax3 = plt.subplots(1,1)
+    fig4, ax4 = plt.subplots(1,1)
 
-    f, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3,2)
     df = pd.read_csv(path, index_col=0, header=None)
     df.columns = ['act', 'pred']
     df['right'] = df['pred'] == df['act']
 
     total_by_class = df.groupby('act').count()
     n_tot = total_by_class['pred'].values
-    n_show = 20
+    n_tot = n_tot.astype(float)
+    n_show = len(n_tot)
 
     
     #number of correct
@@ -35,6 +41,7 @@ def cat_acc(path):
     
     ##hist of accuracies
     sbn.distplot(right_per_arr, kde=False, ax=ax1, bins=25)
+    
 
     ##total number of pictures
     y_tot = np.sort(n_tot)[:-n_show:-1]
@@ -43,12 +50,8 @@ def cat_acc(path):
     sbn.barplot(x_tot_lst, y_tot, ax=ax2, order=x_tot_lst)
 
     ##top/bot 10
-    x_top = x_sorted[:n_show]
-    x_bot = x_sorted[-n_show:]
-    y_top = y_sorted[:n_show]
-    y_bot = y_sorted[-n_show:]
-    sbn.barplot(x_top, y_top, ax=ax3, order=x_top)
-    sbn.barplot(x_bot, y_bot, ax=ax4, order=x_bot)
+    x_sorted_lst = get_breed_labels(x_sorted, breed_dict)
+    sbn.barplot(x_sorted_lst, y_sorted, ax=ax3, order=x_sorted_lst)
     
     ##most predicted/number
     num_pred_df = df.groupby('pred').count()
@@ -58,15 +61,19 @@ def cat_acc(path):
     x_pred_sorted = np.argsort(pred_per_arr)[::-1]
     
     #top/bot pred 10
-    x_num_top = x_pred_sorted[:n_show]
-    x_num_bot = x_pred_sorted[-n_show:]
-    y_num_top = y_pred_sorted[:n_show]
-    y_num_bot = y_pred_sorted[-n_show:]
-    sbn.barplot(x_num_top, y_num_top, ax=ax5, order=x_num_top)
-    sbn.barplot(x_num_bot, y_num_bot, ax=ax6, order=x_num_bot)
+    x_pred_sorted_lst = get_breed_labels(x_pred_sorted, breed_dict)
+    sbn.barplot(x_pred_sorted_lst, y_pred_sorted, ax=ax4, order=x_pred_sorted_lst)
 
-    for ax in f.axes:
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+    #trying another plotting type
+    
+
+    ax1.set_title('Histogram of Accuracies')
+    ax2.set_title('# of Pics per Breed')
+    ax3.set_title('% Accuracy per Class')
+    ax4.set_title('% Class Predicted per Class')
+    ax2.set_xticklabels(ax2.get_xticklabels(), rotation=90)
+    ax3.set_xticklabels(ax3.get_xticklabels(), rotation=90)
+    ax4.set_xticklabels(ax4.get_xticklabels(), rotation=90)
     plt.show()
 
 if __name__ == '__main__':
