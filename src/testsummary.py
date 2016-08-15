d0cc22726164c913168fcae336aa45407946e2de
LKatz <loganzk@gmail.com>
2016-08-15 21:29:07 +0000

    Modified summary file

--- src/testsummary.py
+++ src/testsummary.py
@@ -5,32 +5,36 @@ import json
 import os
 from PIL import Image
 import pandas as pd
+import globes as G
 
-def predict():
-    with open('/Users/lzkatz/Desktop/Galvanize/Capstone/Identifido/aux_files/breed_dict.json') as bd:
-        classes = json.load(bd)
-    model_dir = '/Users/lzkatz/Desktop/Galvanize/Capstone/Identifido/model/deep_nodense/temp_model.hdf5'
-    folder_dir = '/Users/lzkatz/Desktop/Galvanize/Capstone/Identifido/data/test/subtest/'
+def predict(mod_name):
+    model_dir = G.MOD + mod_name + "/temp_model.hdf5"
+    folder_dir = G.TST
 
     model = load_model(model_dir)
     
-    breed_folders = os.listdir(folder_dir)
-
+    breed_folders = sorted(os.listdir(folder_dir))
     img_pred_list = []
 
     for idx, breed in enumerate(breed_folders):
+        print breed
         img_dir = folder_dir + breed + '/'
         img_list = os.listdir(img_dir)
         for img_loc in img_list:
             img_path = img_dir + img_loc
             img = Image.open(img_path)
+            if img.mode != 'RGB':
+                continue
             img = img.resize((model.input_shape[3], model.input_shape[2]))
             img = np.asarray(img, dtype='float32')
             img /= 255.
             img = np.transpose(img, (2,0,1))
             img = np.array([img])
-
-            class_pred = model.predict(img)
+            try:
+                class_pred = model.predict(img)
+            except:
+                print "Pic didn't work"
+                continue
             class_pred = class_pred.flatten()
             sort_pred = np.argsort(class_pred)[:-6:-1]
             if idx == sort_pred[0]:
@@ -50,5 +54,6 @@ def predict():
     return results_df.mean()
 
 if __name__ == '__main__':
-    results = predict()
+    mod = sys.argv[1]
+    results = predict(mod)
     print results
